e9ae58ce4121a551a1c4f5f189f7f0b90e8070df
Jiyul Lee <skyrider1811@gmail.com>
2020-08-22 20:20:36 +0900

    youtube API

--- youtubeAPI.js
+++ youtubeAPI.js
@@ -5,6 +5,7 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
+var player2;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
 
@@ -14,15 +15,35 @@ function onYouTubeIframeAPIReady() {
       'onStateChange': onPlayerStateChange
     }
   });
+
+  player2 = new YT.Player('player2', {
+
+   playerVars:{
+       listType:'playlist',
+       list:'PLBRsMixIwVu6MMFMN_Z1iBuGYT0jtdBsK',
+       rel:0
+   }}
+  );
+  
+player3 = new YT.Player('player3', {
+
+  playerVars:{
+      listType:'playlist',
+      list:'PLBRsMixIwVu7GKSDy8cCnLj15UDieKhhn',
+      rel:0
+  }}
+ );
+ 
 }
+
 
 function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    done = true;
+    event.target.playVideo();
   }
-}
+  
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      done = true;
+    }
+  }
